6cb7623f9aa72ddaf2084873ac86b2b603a2925b
tia_setiawati_sofyan <tiasofyan06@gmail.com>
2021-03-22 15:17:31 +0700

    tugas 1

---
+++ tugas 1/Biodata Singkat.pas
@@ -0,0 +1,27 @@
+{Nama : Tia Setiawati Sofyan}
+{NPM : 20.14.1.0063}
+{Nama Program : Biodata}
+
+Program Biodata;
+uses crt;
+
+var
+nama,npm,alamat,ttl,nohp,motto :string;
+
+begin
+write('1. Nama : ');
+readln(nama);
+write('2. NPM : ');
+readln(npm);
+write('3. Alamat : ');
+readln(alamat);
+write('4. TTL : ');
+readln(ttl);
+write('5. No.HP : ');
+readln(nohp);
+write('6. Motto ; ');
+readln(motto);
+
+
+readln;
+end.
